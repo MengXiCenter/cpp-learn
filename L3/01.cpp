9739bd70abaa4e6a8e08a60b02d0590481d73555
me <me@zhunote.cn>
2024-05-25 02:08:17 +0000

    I Love HUAWEI

--- L3/01.cpp
+++ L3/01.cpp
@@ -1,5 +1,15 @@
 #include<bits/stdc++.h>
+#include <cstdio>
 using namespace std;
 int main(){
-    
+    char c[201],a,b;
+    gets(c);
+    cin>>a>>b;
+    for(int i=0;c[i]!='\0';i++){
+        if(c[i] == a){
+            cout<<b;
+        }else{
+            cout<<c[i];
+        }
+    }
 }
